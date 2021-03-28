60f647c6190fe6ebae05fd4b680640c0e26f5681
Karen Gomes <kngs@ic.ufal.br>
2021-03-27 21:33:21 -0300

    add axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,21 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL_API,
+  baseURL: process.env.REACT_APP_BASE_URL_API,
 });
-
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = sessionStorage.getItem("bl-token");
-//     if (token) {
-//       config.headers.authorization = token;
-//     }
-
-//     return config;
-//   },
-//   (error) => {
-//     Promise.reject(error);
-//   }
-// );
 
 export default api;
