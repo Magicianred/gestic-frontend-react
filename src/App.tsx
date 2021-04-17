662c089f46a7fd5d1a28519bdbab9c0c1c80188b
Pedro Javier <pjpc@ic.ufal.br>
2021-04-16 22:38:47 -0300

    fix: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Navigation } from './navigation';
 import TemplateProvider from './providers/TemplateProvider';
 import { AuthProvider } from './providers/AuthProvider';
 
 const App = () => (
-  <AuthProvider>
-    <TemplateProvider>
-      <Navigation />
-    </TemplateProvider>
-  </AuthProvider>
+  <Router>
+    <AuthProvider>
+      <TemplateProvider>
+        <Navigation />
+      </TemplateProvider>
+    </AuthProvider>
+  </Router>
 );
 
 export default App;
